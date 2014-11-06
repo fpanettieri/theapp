b809b0a630f120d5977422113408ee90e3f1b566
Fabio Panettieri <fpanettieri@gmail.com>
2014-11-06 10:43:56 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exodia::Application.config.secret_key_base = 'ed8f6998158a0acfe6bf2342fa76bee9aed994920b1a9e3ae104ebfc67e7c8fcb93ad8821288924d68e7223ae46e1b1e27b54a3cdb1f4226dc0e7f8e68262f96'
