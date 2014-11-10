8a67dd9792fc1df8f0554f6566d19e4ea23ba260
Fabio Panettieri <fpanettieri@gmail.com>
2014-11-09 23:35:11 -0800

    map working as expected

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,14 +1,23 @@
 class CommunitiesController < ApplicationController
+  before_action :prepare_paths, only: [:map]
+  
   def map
+    @community = params[:name]
     # replace this filter 
     @loc = hot_locations
     gon.locations = @loc.as_json(:only => [:id, :latitude, :longitude])
-    gon.locations_path = locations_path
   end
   
   private
   def hot_locations
     # TODO: implement real "hot" filter
     Location.all
+  end
+  
+  def prepare_paths
+    gon.locations_path = locations_path
+    gon.list_path = url_for controller: 'communities', action: 'list'
+    gon.map_path = url_for controller: 'communities', action: 'map'
+    gon.feed_path = url_for controller: 'communities', action: 'feed'
   end
 end
