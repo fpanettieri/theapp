0073a3d6afa07cb1dc14236a2df4add35990d5f1
Fabio Panettieri <fpanettieri@gmail.com>
2014-11-13 18:08:11 -0800

    New location screen

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,4 +4,20 @@ class LocationsController < ApplicationController
     @location.distance = 12.32 # calculate this?
     @topics = @location.topics.order("votes desc")
   end
+  
+  def new
+    @location = Location.new
+  end
+  
+  def create
+    location = Location.new
+    location.name = params[:name]
+    location.longitude = 0
+    location.latitude = 0
+    location.address = params[:address]
+    location.hours = params[:hours]
+    location.detail = params[:detail]
+    location.save
+    gon.return_url = "/c/bitcoin/list"
+  end
 end
